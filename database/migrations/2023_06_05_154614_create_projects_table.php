4405f827abc35709f42d52d83a2bdef7ab095e40
Nicowski25 <nicola.faedo0610@gmail.com>
2023-06-05 18:56:08 +0200

    created mingration and db

--- database/migrations/2023_06_05_154614_create_projects_table.php
+++ database/migrations/2023_06_05_154614_create_projects_table.php
@@ -15,6 +15,12 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string('title', '50')->unique();
+            $table->text('description');
+            $table->integer('duration');
+            $table->string('status')->default('inprogress');
+            $table->date('start_date');
+            $table->date('end_date')->nullable();
             $table->timestamps();
         });
     }
