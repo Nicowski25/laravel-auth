bd859f3f488ecf76f98baa08709ec76e4aab89d4
Nicowski25 <nicola.faedo0610@gmail.com>
2023-06-05 17:50:49 +0200

    admin.blade and project model creation

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,9 @@ Route::get('/', function () {
 });
 
 
-Route::get('/admin', [DashboardController::class, 'index'])->name(('dashboard'));
+Route::middleware(['auth', 'verified'])->prefix('admin')->name('admin.')->group(function () {
+    Route::get('/', [DashboardController::class, 'index'])->name(('dashboard'));
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -28,4 +30,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
